9cf95cd05b7b241da3afb21a1b0dc272a4ad00d5
Jang-Donghyeok <68702805+Jang-Donghyeok@users.noreply.github.com>
2021-01-20 23:55:16 +0900

    a

---
+++ 4963.py
@@ -0,0 +1,37 @@
+import sys
+from collections import deque
+input = sys.stdin.readline
+
+def find(w, h):
+    direction = [(1, 0), (-1, 0), (0, 1), (0, -1), (1, 1), (1, -1), (-1, 1), (-1, -1)]
+
+    q = deque()
+    q.append((w,h))
+    land[w][h] = 0
+    while q:
+        place = q.popleft()
+        for i in range(8):
+            nx = place[0] + direction[i][0]
+            ny = place[1] + direction[i][1]
+
+            if (0 <= nx < h) and (0 <= ny < w):
+                if land[nx][ny] == 1:
+                    land[nx][ny] = 0
+                    q.append((nx,ny))
+
+
+while True:
+    w, h = map(int, input().split())
+    count = 0
+    land = []
+    if w == 0 and h == 0:
+        break
+    for _ in range(h):
+        cnt = list(map(int, input().split()))
+        land.append(cnt)
+    for i in range(h):
+        for j in range(w):
+            if land[i][j] != 0:
+                count += 1
+                find(i,j)
+    print(count)
