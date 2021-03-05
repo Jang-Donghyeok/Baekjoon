9f8dcf1eb22190272c7c8cb60879b5307655f79a
Jang-Donghyeok <68702805+Jang-Donghyeok@users.noreply.github.com>
2021-03-05 22:41:29 +0900

    a

--- 6588-골드바흐의 추측.py
+++ 6588-골드바흐의 추측.py
@@ -1,17 +1,16 @@
 import sys
 input = sys.stdin.readline
 
-sosu = [0] *1000001
-sosu[1] = 1
-for i in range(2,1001):
+sosu = [1] * 1000001
+sosu[1] = 0
+for i in range(2, 1001):
     for j in range(i*2, 1000001, i):
-        sosu[j] = 1
+        sosu[j] = 0
 while True:
     a = int(input())
     if a == 0:
         break
-    b = a // 2
-    for j in range(1, b+1):
-        if sosu[a-j] ==0 and sosu[j] == 0:
-            print(a, '=' , j, '+',a-j)
+    for j in range(1, a+1):
+        if sosu[a-j] and sosu[j]:
+            print(a, '=', j, '+', a-j)
             break
