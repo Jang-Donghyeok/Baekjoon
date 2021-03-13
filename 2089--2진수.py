d6c5257cc29fba181a6c162ff0c226d20b571bf4
Jang-Donghyeok <68702805+Jang-Donghyeok@users.noreply.github.com>
2021-03-13 21:52:19 +0900

    a

--- 2089--2진수.py
+++ 2089--2진수.py
@@ -1,2 +1,14 @@
-n = input()
-print(format(n, '-b'))
+n = int(input())
+def decimal(n,k):
+    if n == 0:
+        return '0'
+    else:
+        if n % -2 == 0:
+            return decimal(n // k, k) + '0'
+        else:
+            return decimal(n // k+1,k) + '1'
+result = decimal(n,-2)
+if result == '0':
+    print('0')
+else:
+    print(result[1:])
