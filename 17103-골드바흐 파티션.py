1903e733a28fd6a3554c1aacf51a487e74345fec
Jang-Donghyeok <68702805+Jang-Donghyeok@users.noreply.github.com>
2021-03-16 22:55:29 +0900

    a

--- 17103-골드바흐 파티션.py
+++ 17103-골드바흐 파티션.py
@@ -4,12 +4,12 @@ input = sys.stdin.readline
 n = int(input())
 nums = [int(input()) for _ in range(n)]
 m = max(nums)
-sosu = [False,False] + [True] * (m-1)
+sosu = [0,0] + [1] * (m-1)
 for i in range(2, int(m**0.5)+1):
     if sosu[i]:
         for j in range(i+i, m+1, i):
             if sosu[j]:
-                sosu[j] = False
+                sosu[j] = 0
 for num in nums:
     count = 0
     for i in range((num//2) +1):
