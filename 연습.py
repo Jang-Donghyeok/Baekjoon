66a8b5f5d196e9b8e4434fcf165f5e2771b11ae9
Jang-Donghyeok <68702805+Jang-Donghyeok@users.noreply.github.com>
2021-03-30 01:12:49 +0900

    a

--- 연습.py
+++ 연습.py
@@ -1,19 +1,8 @@
-def pivoting(mat):
-    row_idx = list(range(len(mat)))
-    col_idx = len(mat[0])
-    pivot_mat = []
-    for c in range(col_idx):
-        rows_with_nonzero = [r for r in row_idx if mat[r][c] != 0]
-        if rows_with_nonzero:
-            pivot = rows_with_nonzero[0]
-            for idx in rows_with_nonzero:
-                pivot_mat.append(mat[idx])
-                row_idx.remove(idx)
-    return pivot_mat
-
-A = [[3, 2, -1, -15],
-       [5, 3, 2, 0],
-       [3, 1, 3, 11],
-       [6, -4, 2, 30]]
-from pprint import pprint
-pprint(pivoting(A))
+while True:
+    a = int(input("a : "))
+    b = int(input("b : "))
+    temp = a+b-b*b
+    if temp >= 0:
+        print(temp)
+    else:
+        print("음수")
