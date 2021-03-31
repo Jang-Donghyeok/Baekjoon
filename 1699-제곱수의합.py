87ccce011561d1051954da997a904302595c9fec
Jang-Donghyeok <68702805+Jang-Donghyeok@users.noreply.github.com>
2021-03-31 23:57:40 +0900

    a

---
+++ 1699-제곱수의합.py
@@ -0,0 +1,11 @@
+import math
+
+n = int(input())
+dp = [1] * (n+1)
+sqrt = [i*i for i in range(1,317)]
+for i in range(1,n+1):
+    if int(math.sqrt(i)) in sqrt:
+        dp[i] =
+    else:
+        dp[i] = dp[i-1] + 1
+print(dp)
