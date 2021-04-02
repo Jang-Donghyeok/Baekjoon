c48d28b43e0ba3cf4c7f48bab7d7679bf2d433e8
Jang-Donghyeok <68702805+Jang-Donghyeok@users.noreply.github.com>
2021-04-02 22:56:14 +0900

    a

--- 15988- 1,2,3더하기 3.py
+++ 15988- 1,2,3더하기 3.py
@@ -2,13 +2,13 @@ import sys
 input = sys.stdin.readline
 
 n = int(input())
-dp= [0 for i in range(100001)]
+dp = [0 for i in range(1000001)]
 dp[1] = 1
 dp[2] = 2
 dp[3] = 4
 
-for i in range(4,100001):
-       dp[i] = dp[i-1] + dp[i-2] + dp[i-3]
+for i in range(4,1000001):
+       dp[i] = dp[i-1] % 1000000009 + dp[i-2] % 1000000009 + dp[i-3] % 1000000009
 
 for i in range(n):
     num = int(input())
