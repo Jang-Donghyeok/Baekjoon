8f82fe7dbbb5c161650c4740b243070ac48e9e8d
Jang-Donghyeok <68702805+Jang-Donghyeok@users.noreply.github.com>
2021-04-04 23:35:09 +0900

    a

--- 1149-RGB거리.py
+++ 1149-RGB거리.py
@@ -2,7 +2,13 @@ import sys
 input = sys.stdin.readline
 
 n = int(input())
-N = []
+dp = []
 for i in range(n):
-    R = list(map(int, input()))
-    N.append(R)
+    dp.append(list(map(int, input().split())))
+for i in range(1,n):
+    dp[i][0] = min(dp[i-1][1], dp[i-1][2]) + dp[i][0]
+    dp[i][1] = min(dp[i-1][0], dp[i-1][2]) + dp[i][1]
+    dp[i][2] = min(dp[i-1][0], dp[i-1][1]) + dp[i][2]
+print(min(dp[n-1]))
+
+
