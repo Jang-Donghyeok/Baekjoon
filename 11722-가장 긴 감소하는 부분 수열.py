e92d7c4e05cdaaedaec56a41610c1bc00539113c
Jang-Donghyeok <68702805+Jang-Donghyeok@users.noreply.github.com>
2021-04-09 23:06:17 +0900

    a

--- 11722-가장 긴 감소하는 부분 수열.py
+++ 11722-가장 긴 감소하는 부분 수열.py
@@ -3,9 +3,10 @@ input = sys.stdin.readline
 
 n = int(input())
 A = list(map(int, input().split()))
-dp = [i for i in A]
+dp = [0]*n
 for i in range(len(A)):
     for j in range(i):
-        if A[j] < A[i]:
-            dp[i] = max(dp[i], dp[j]+A[i])
+        if A[j] > A[i] and dp[j] > dp[i]:
+            dp[i] = dp[j]
+    dp[i] += 1
 print(max(dp))
