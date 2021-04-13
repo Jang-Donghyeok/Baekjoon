a4e33e7982a0c1a8f2ea4507ccf2a14d17fcaf1a
Jang-Donghyeok <68702805+Jang-Donghyeok@users.noreply.github.com>
2021-04-13 21:58:25 +0900

    a

--- 13398-연속합 2.py
+++ 13398-연속합 2.py
@@ -5,7 +5,7 @@ n = int(input())
 A = list(map(int, input().split()))
 dp = [[0, 0] for i in range(n)]
 dp[0][0] = A[0]
-m = -100000000
+m = A[0]
 if n > 1:
     for i in range(1, n):
         dp[i][0] = max(dp[i-1][0]+A[i], A[i])
