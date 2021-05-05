53e6c9b5ccfffd44ef13e50f4d0f890c713c7585
Jang-Donghyeok <68702805+Jang-Donghyeok@users.noreply.github.com>
2021-05-05 17:57:05 +0900

    a

--- 연습.py
+++ 연습.py
@@ -1,28 +1,22 @@
 import random
-list = []
-list2 = []
+
+lucky = random.randint(10000,99999)
+n = input("5자리 복권번호를 입력하시오: ")
 count = 0
+lucky = str(lucky)
+print("당첨번호는", lucky, "입니다.")
+
 for i in range(5):
-    num = random.randint(0,9)
-    if num not in list:
-        list.append(random.randint(0, 9))
-while len(list2) != 5:
-    n = int(input("복권 번호를 입력하세요:"))
-    if n not in list2:
-        list2.append(n)
-    else:
-        print("중복된 수입니다.")
-for i in list:
-    for j in list2:
-        if i == j:
-            count += 1
-if count >= 5:
-    print("백만원입니다.")
-elif count >= 4:
-    print("50만원입니다.")
-elif count >= 3:
-    print("30만원입니다.")
-elif count >= 2:
-    print("5만원입니다.")
+    if lucky[i] == n[i]:
+        count += 1
+
+if count == 5:
+    print("상금은 100만원입니다.")
+elif count == 4:
+    print("상금은 50만원입니다.")
+elif count == 3:
+    print("상금은 30만원입니다.")
+elif count ==2:
+    print("상금은 5만원입니다.")
 else:
-    print("꽝")
+    print("꽝입니다.")
