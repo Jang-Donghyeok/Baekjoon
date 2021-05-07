48a125ca5ca5f95b10b4a2fdca921ac70ecc8282
Jang-Donghyeok <68702805+Jang-Donghyeok@users.noreply.github.com>
2021-05-07 18:14:36 +0900

    a

--- 연습.py
+++ 연습.py
@@ -1,22 +1,3 @@
-import random
-
-lucky = random.randint(10000,99999)
-n = input("5자리 복권번호를 입력하시오: ")
-count = 0
-lucky = str(lucky)
-print("당첨번호는", lucky, "입니다.")
-
-for i in range(5):
-    if lucky[i] == n[i]:
-        count += 1
-
-if count == 5:
-    print("상금은 100만원입니다.")
-elif count == 4:
-    print("상금은 50만원입니다.")
-elif count == 3:
-    print("상금은 30만원입니다.")
-elif count ==2:
-    print("상금은 5만원입니다.")
-else:
-    print("꽝입니다.")
+domains = {"kr":"대한민국", "sk":"슬로바키아","no":"노르웨이","us":"미국","jp":"일본","hu":"헝가리","de":"독일"}
+for k, v in domains.items():
+    print(k, ": ", v)
