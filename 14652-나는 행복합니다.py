73fccf4d1c45722758ac2d7e198bd90f9f5bd2e8
Jang-Donghyeok <68702805+Jang-Donghyeok@users.noreply.github.com>
2021-06-09 00:07:08 +0900

    a

--- 14652-나는 행복합니다.py
+++ 14652-나는 행복합니다.py
@@ -1,9 +1,4 @@
-N, M, k = map(int, input().split())
-A = [ for i in range(M)]*N]
-print(A)
-k = 0
-for i in range(N):
-    for j in range(M):
-        A[i][j] = k
-        k += 1
-print(A)
+N, M, K = map(int, input().split())
+n = K // M
+m = K % M
+print(n, m)
