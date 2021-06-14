d36b6228036f82b88deb5464883116c0c401fe45
Jang-Donghyeok <68702805+Jang-Donghyeok@users.noreply.github.com>
2021-06-15 00:05:49 +0900

    a

--- 연습.py
+++ 연습.py
@@ -1,5 +1,28 @@
-import numpy as np
+import random
 
-b = np.array([[1,2,3],[4,5,6],[7,8,9]])
-print(b)
-print(b[0][2])
+def getRandom(A,B):
+    n = random.randint(ord(A),ord(B))
+    print(chr(n))
+def getRandomLower():
+    n = random.randint(97,122)
+    print(chr(n))
+def getRandomUpper():
+    n = random.randint(65,90)
+    print(chr(n))
+def getRandomDigitCharacter():
+    n = random.randint(0,100)
+    print(n)
+def getRandomASCII():
+    n = random.randint(32,127)
+    n = chr(n)
+    print(n)
+
+A = input("문자를 입력하시오.")
+B = input("문자를 입력하시오.")
+
+getRandom(A,B)
+getRandomLower()
+getRandomUpper()
+getRandomDigitCharacter()
+getRandomASCII()
+
