042ad3fbd2673ae09f7b6d513234511c4bd78750
Jang-Donghyeok <68702805+Jang-Donghyeok@users.noreply.github.com>
2022-01-14 23:03:02 +0900

    Baekjoon-3003 킹, 퀸, 룩, 비숍, 나이트, 폰

---
+++ Java/src/KingQueenLookBishopKnightPqwn.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class KingQueenLookBishopKnightPqwn {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int[] cnt = new int[6];
+        int[] full = new int[]{1,1,2,2,2,8};
+        for(int i=0; i<6; i++){
+            cnt[i] = sc.nextInt();
+        }
+        for(int i=0; i<full.length; i++){
+            cnt[i] = full[i] - cnt[i];
+            System.out.println(cnt[i]);
+        }
+    }
+}
